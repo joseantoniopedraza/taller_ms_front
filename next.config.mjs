b8eee3397c58d0550d0d074a71acc754d37353e8
Jose Antonio Pedraza <joseantoniopedraza@MacBook-Pro-de-Jose.local>
2025-08-02 13:32:21 -0400

    feat(clients): enable post

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/clients',
+        destination: 'http://taller-ms-persistence:3001/clients/',
+      },
+      {
+        source: '/api/clients/',
+        destination: 'http://taller-ms-persistence:3001/clients/',
+      },
+      {
+        source: '/api/:path*',
+        destination: 'http://taller-ms-persistence:3001/:path*',
+      },
+    ];
+  },
+};
 
 export default nextConfig;
